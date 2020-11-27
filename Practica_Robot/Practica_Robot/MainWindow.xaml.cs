c15bd7ad2538beddbf04bcfb3e9b08c93f7a0fd7
Joan Arbat <a1804551@institutmontilivi.cat>
2020-11-27 02:59:25 +0100

    robot 1

---
+++ Practica_Robot/Practica_Robot/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Practica_Robot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        DispatcherTimer timer2;
+        Robot robot;
+        int count;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+          
+            robot = new Robot();
+            robot.WIDTH = (int)canvas.Width;
+            robot.HEIGHT = (int)canvas.Height;
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            count = 0;
+        }
+        
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (Image image in robot.Robots)
+            {
+                move(image);
+                sitoca(image);
+                
+            }
+            
+            tb_nIntents.Text = "Numero d'intents: " + count;
+        }
+
+        private void sitoca(Image sender)
+        {
+            int esquerrarob = (int)Canvas.GetLeft(sender);
+            int adaltrob = (int)Canvas.GetTop(sender);
+            int esquerra = (int)Canvas.GetLeft(robot.Tresor);
+            int adalt = (int)Canvas.GetTop(robot.Tresor);
+            if(adalt==adaltrob && esquerra==esquerrarob)
+            {
+                timer.Stop();
+                btn_Incia.Content = "Final";
+            }
+        }
+
+        private void btn_Incia_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+         
+            Random r = new Random();
+            foreach (Image image in robot.Robots)
+            {
+                 Canvas.SetTop(image, (double)r.Next(robot.Ampladarob, robot.HEIGHT - (robot.Ampladarob+1)));
+                 Canvas.SetLeft(image, (double)r.Next(robot.Llargadarob, robot.WIDTH - (robot.Ampladarob+1)));
+               
+                canvas.Children.Add(image);
+            }
+           
+            Canvas.SetTop(robot.Tresor, (double)r.Next(robot.Ampladarob, robot.HEIGHT - (robot.Ampladarob + 1)));
+            Canvas.SetLeft(robot.Tresor, (double)r.Next(robot.Llargadarob, robot.WIDTH - (robot.Ampladarob + 1)));
+            canvas.Children.Add(robot.Tresor);
+
+        }
+        private void move(Image sender)
+        {
+           
+            
+
+            int esquerra=(int) Canvas.GetLeft(sender);
+            int adalt = (int)Canvas.GetTop(sender);
+            if (robot.Direccio == DireccioRobot.Nort)
+            {
+                count++;
+                adalt -= 25;
+                if (adalt <=  robot.Llargadarob)
+                {
+                    setDir();
+                  
+                }
+            }
+
+            else if (robot.Direccio == DireccioRobot.Sud)
+            {
+                count++;
+                adalt += 25;
+                if (adalt >= (robot.HEIGHT -(robot.Llargadarob * 2)))
+                {
+                    setDir();
+
+                }
+
+            }
+            else  if (robot.Direccio == DireccioRobot.Est)
+            {
+                count++;
+                esquerra -= 25;
+                if (esquerra <= robot.Ampladarob)
+                {
+                    setDir();
+
+                }
+            }
+            else if (robot.Direccio == DireccioRobot.Oest)
+            {
+                count++;
+                esquerra += 25;
+                if (esquerra >= (robot.WIDTH - (robot.Ampladarob*2)))
+                {
+                    setDir();
+
+                }
+
+            }
+            Canvas.SetLeft(sender, esquerra);
+            Canvas.SetTop(sender, adalt);
+        }
+
+        public void setDir()
+        {
+            Random r = new Random();
+            int randomRest = r.Next(0, 4);
+
+            if (randomRest == 0)
+            {
+                robot.Direccio = DireccioRobot.Sud;
+            }
+            else if( randomRest ==1)
+            {
+                robot.Direccio = DireccioRobot.Nort;
+            }
+            else if (randomRest == 2)
+            {
+                robot.Direccio = DireccioRobot.Est;
+            }
+            else if (randomRest == 3)
+            {
+                robot.Direccio = DireccioRobot.Oest;
+            }
+        }
+    }
+}
---
+++ Practica_Robot/Practica_Robot/Robot.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Practica_Robot
+{
+    class Robot
+    {
+        private int wIDTH = 785;
+        private int hEIGHT = 330;
+        private const int ampladarob = 120;
+        private const int llargadarob = 152;
+        public const int NROBOTS = 2;
+        Image tresor;
+        DireccioRobot direccio;
+        private List<Image> robots;
+        Point cap;
+
+        public Point Cap { get => cap; set => cap = value; }
+
+        public DireccioRobot Direccio { get => direccio; set => direccio = value; }
+
+        public int WIDTH { get => wIDTH; set => wIDTH = value; }
+        public int HEIGHT { get => hEIGHT; set => hEIGHT = value; }
+        public List<Image> Robots { get => robots; set => robots = value; }
+
+        public int Ampladarob
+        {
+            get => ampladarob;
+        }
+        public int Llargadarob
+        {
+            get => llargadarob;
+        }
+        public Image Tresor { get => tresor; set => tresor = value; }
+
+        public void moure()
+        {
+
+            if (direccio == DireccioRobot.Nort)
+            {
+                cap.Y--;
+                if (cap.Y == -1)
+                {
+                    cap.Y = HEIGHT - 1;
+                }
+            }
+
+            else if (direccio == DireccioRobot.Sud)
+            {
+                cap.Y++;
+                if (cap.Y == HEIGHT)
+                {
+                    cap.Y = 0;
+                }
+
+            }
+            if (direccio == DireccioRobot.Est)
+            {
+                cap.X--;
+                if (cap.X == -1)
+                {
+                    cap.X = WIDTH - 1;
+                }
+            }
+            else if (direccio == DireccioRobot.Oest)
+            {
+                cap.X++;
+                if (cap.X == WIDTH)
+                {
+                    cap.X = 0;
+                }
+
+            }
+        }
+
+        public void RandomIncial()
+        {
+            Random r = new Random();
+
+
+
+
+        }
+        public Robot()
+        {
+            robots = new List<Image>();
+            Image robot2;
+            BitmapImage bitmapImage;
+
+
+            for (int i = 0; i < NROBOTS; i++)
+            {
+                robot2 = new Image();
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri("/imatges/robo.png", UriKind.Relative);
+                bitmapImage.EndInit();
+                robot2.Source = bitmapImage;
+                robot2.Width = ampladarob;
+                robot2.Height = llargadarob;
+                robots.Add(robot2);
+
+
+            }
+            tresor = new Image();
+            bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri("/imatges/cofre.png", UriKind.Relative);
+            bitmapImage.EndInit();
+            tresor.Source = bitmapImage;
+            tresor.Width = ampladarob;
+            tresor.Height = llargadarob;
+            tresor.Source = bitmapImage;
+        }
+    }
+
+
+        public enum DireccioRobot
+        {
+            Sud,
+            Nort, Est, Oest
+
+        }
+    }
